a96040a533955181017f930282a75d079dbe053a
Lee Callender <leecallender@udhcp-macvpn-199.public.engin.umich.edu>
2009-08-10 10:25:21 -0400

    Added capistrano to the project

---
+++ config/deploy.rb
@@ -0,0 +1,53 @@
+set :application, "computer-poker-analysis"
+set :repository,  "ssh://deepmaize@aifa.eecs.umich.edu:~/repos/notetaker.git"
+set :domain, "aifa.eecs.umich.edu"
+
+set :mongrel_conf, "#{current_path}/config/mongrel_cluster.yml"
+set :scm, :git
+set :deploy_via, :remote_cache
+
+ssh_options[:paranoid] = false
+
+set :user, 'deepmaize'
+set :runner, 'deepmaize'
+set :use_sudo, false
+
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+# moves over server config files
+task :update_config, :roles => [:app] do
+  run "cp -Rf #{shared_path}/config/* #{release_path}/config/"
+end
+after 'deploy:update_code', :update_config
+
+# mongrel-based overrides of the default tasks
+
+namespace :deploy do
+  namespace :mongrel do
+    [ :stop, :start, :restart ].each do |t|
+      desc "#{t.to_s.capitalize} the mongrel appserver"
+      task t, :roles => :app do
+        #invoke_command checks the use_sudo variable to determine how to run the mongrel_rails command
+        invoke_command "mongrel_rails cluster::#{t.to_s} -C #{mongrel_conf}", :via => run_method
+      end
+    end
+  end
+
+  desc "Custom restart task for mongrel cluster"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    deploy.mongrel.restart
+  end
+
+  desc "Custom start task for mongrel cluster"
+  task :start, :roles => :app do
+    deploy.mongrel.start
+  end
+
+  desc "Custom stop task for mongrel cluster"
+  task :stop, :roles => :app do
+    deploy.mongrel.stop
+  end
+
+end
