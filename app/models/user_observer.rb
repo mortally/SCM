5d71bf53230e3df47dd1adea092a9aaa8a0dce39
Lee Callender <leecallender@udhcp-macvpn-199.public.engin.umich.edu>
2009-08-10 11:43:48 -0400

    more shit

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -1,6 +1,6 @@
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
-    
+    #This doesn't appear to work on aifa
     #Admin::AdminNotifier.deliver_create_account_notification(user)
   end
 end
