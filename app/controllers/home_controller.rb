83d5db000f61ca3fb99ce677374e7d1f45e4e1e0
Lee Callender <leecallender@udhcp-macvpn-124.public.engin.umich.edu>
2009-08-13 19:01:22 -0400

    added some images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,12 @@
 class HomeController < ApplicationController
 
   def index    
-    #@clean_sample_count = EightPlayerPayoff.count
-    #@sample_count = Sample.count
-    #@active_simulation_count = Simulation.active.count
-    #@complete_simulation_count = Simulation.complete.count
-    #@active_scheduler_count = GameScheduler.active.count
-    #@scheduler_count = GameScheduler.count
-    @clean_sample_count = 1
-    @sample_count = 2
-    @active_simulation_count = 3
-    @complete_simulation_count = 4
-    @active_scheduler_count = 5
-    @scheduler_count = 6
+    @clean_sample_count = ThreePlayerPayoff.count
+    @sample_count = Sample.count
+    @active_simulation_count = Simulation.active.count
+    @complete_simulation_count = Simulation.complete.count
+    @active_scheduler_count = GameScheduler.active.count
+    @scheduler_count = GameScheduler.count
   end
 
 end
