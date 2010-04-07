6d01cba78db62b4b422effdbf5d608f14221e84f
Augustus Odena <odenaaugustus@gmail.com>
2010-04-06 21:28:11 -0400

    fixed that thing because it was messing with me - I need to get this log working

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,7 @@ RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
 ENV['RAILS_ENV'] ||= 'production'
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-if "irb" == $0
-  ActiveRecord::Base.logger = Logger.new(STDOUT)
-end
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
