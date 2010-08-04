0d141d6deb762f343e6b8196490742d2b9e2c62b
dong0 <dong0@dong0-laptop.(none)>
2010-08-04 13:10:38 -0400

    Migration for games_n_player_profiles table added

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ActiveRecord::Base
   belongs_to :server
   
   has_and_belongs_to_many :strategies
+  has_and_belongs_to_many :n_player_profiles
   has_many :game_schedulers
   
 end
--- db/migrate/20100728190752_migrate_to_egt.rb
+++ db/migrate/20100728190752_migrate_to_egt.rb
@@ -74,6 +74,14 @@ class MigrateToEgt < ActiveRecord::Migration
     add_index :deviation_schedulers, :n_player_profile_id 
     add_index :deviation_schedulers, :strategy_id
     
+    create_table :games_n_player_profiles, :id=>false do |t|
+      t.references :game
+      t.references :n_player_profile
+    end
+    
+    add_index :games_n_player_profiles, :game_id
+    add_index :games_n_player_profiles, :n_player_profile_id
+    
 ##### Data migration #####
     
     # Every game in TAC/SCM has size of 3
@@ -143,6 +151,18 @@ class MigrateToEgt < ActiveRecord::Migration
   		  npp.save! 		
     	end
     end
+    
+    # Migration for Games_N_Player_Profiles
+    ThreePlayerGame.all.each do |game|
+    	tp_profiles = game.three_player_profiles
+    	
+    	tp_profiles.each do |tpp|
+    		np_id = map[tpp.id]
+    		game.n_player_profiles << NPlayerProfile.find(np_id)
+    	end
+    	game.save!
+    end
+    
     
     # Clean up obsolete tables
     drop_table :three_player_adjusted_payoffs
